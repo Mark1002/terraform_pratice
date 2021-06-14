85e9b55a19b36cbd40788d357a94057ffacbbecf
mark1002 <mark.chang@mile.cloud>
2021-06-14 10:39:35 +0800

    fix output

--- modules/instances/outputs.tf
+++ modules/instances/outputs.tf
@@ -1,9 +1,9 @@
 output "tf-instance-1" {
   description = "tf-instance-1 vm names."
-  value       = "module.instances.tf-instance-1"
+  value       = "google_compute_instance.tf-instance-1"
 }
 
 output "tf-instance-2" {
   description = "tf-instance-2 vm names."
-  value       = "module.instances.tf-instance-2"
+  value       = "google_compute_instance.tf-instance-2"
 }
